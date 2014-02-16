57c2670a4f70560995358dd2dcbc1d535b0e749c
clee749 <charles_lee@alumni.brown.edu>
2014-02-15 19:37:35 -0800

    Fix ProximityBomb dropped by Pyro
    When ProximityBombs are dropped by Pyros, they will not detonate on the
    source Pyro until enough time has passed

--- src/mapobject/ProximityBomb.java
+++ src/mapobject/ProximityBomb.java
@@ -23,11 +23,14 @@ public class ProximityBomb extends MapObject {
   public static final double SPLASH_DAMAGE_RADIUS = 1.0;
   public static final double EXPLOSION_RADIUS = RADIUS * 2;
   public static final double EXPLOSION_TIME = 0.75;
+  public static final double TIME_TO_FULLY_ARM = 5.0;
 
   private final MapObject source;
   private final int damage;
   private int frame_num;
   private double frame_time_left;
+  private boolean is_fully_armed;
+  private double time_until_fully_armed;
 
   public ProximityBomb(MapObject source, int damage, Room room, double x_loc, double y_loc) {
     super(RADIUS, room, x_loc, y_loc);
@@ -35,6 +38,12 @@ public class ProximityBomb extends MapObject {
     this.damage = damage;
     frame_num = (int) (Math.random() * 9);
     frame_time_left = SECONDS_PER_FRAME;
+    if (source.getType().equals(ObjectType.Pyro)) {
+      time_until_fully_armed = TIME_TO_FULLY_ARM;
+    }
+    else {
+      is_fully_armed = true;
+    }
   }
 
   @Override
@@ -58,16 +67,21 @@ public class ProximityBomb extends MapObject {
 
   @Override
   public MapObject doNextAction(MapEngine engine, double s_elapsed) {
+    frame_time_left -= s_elapsed;
     if (frame_time_left < 0.0) {
       ++frame_num;
       frame_time_left = SECONDS_PER_FRAME;
     }
-    else {
-      frame_time_left -= s_elapsed;
+
+    if (!is_fully_armed) {
+      time_until_fully_armed -= s_elapsed;
+      if (time_until_fully_armed < 0.0) {
+        is_fully_armed = true;
+      }
     }
 
     for (Pyro pyro : room.getPyros()) {
-      if (isUnitInRange(pyro)) {
+      if ((is_fully_armed || !pyro.equals(source)) && isUnitInRange(pyro)) {
         return handleDetonation(pyro);
       }
     }
